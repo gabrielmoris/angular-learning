f55952d007ec15c3900fc99dd32bb51a8f592898
gabriel <gabrielcmoris@gmail.com>
2024-09-28 15:09:20 +0200

    first sight angular app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,8 +6,22 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Gabriel';
+  myBtn = 'My Button';
+
+  // attribute binding
+  isDisabled = true;
+  angularImage =
+    'https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg';
+
+  // style binding
+  bgColor = 'green';
+  titleColor = 'white';
+  description = 'font-size: 9px; color:red'; // this is actually a cleaner way
+
+  // Class binding
+  textColor = 'yes!';
 }
